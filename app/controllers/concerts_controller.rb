a68fd31eafb7527a4961268a1f1cbda4676aab2a
majaliju <majalijunyc@gmail.com>
2022-12-22 16:16:43 -0500

    concerts controller updated

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,2 +1,11 @@
 class ConcertsController < ApplicationController
+  def index
+    concerts = Concert.all
+    render json: concerts
+  end
+
+  def show
+    concert = Concert.find_by(id: params[:id])
+    render json: concert, status: 200
+  end
 end
