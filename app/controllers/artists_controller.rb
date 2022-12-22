6e1f03bd1166519a9b50b9d630e41b69ec80aaf2
majaliju <majalijunyc@gmail.com>
2022-12-22 16:16:24 -0500

    artists controller updated

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,13 @@
 class ArtistsController < ApplicationController
+  
+  def index
+    artists = Artist.all
+    render json: artists
+  end
+
+  def show
+    artist = Artist.find_by(id: params[:id])
+    render json: artist, status: 200
+  end
+
 end
