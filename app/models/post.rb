d673135d7c17a20a9822f10b37c3063a623787a0
majaliju <majalijunyc@gmail.com>
2022-12-22 16:15:05 -0500

    imported post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  belongs_to :concert
+
+  validates :body, presence: true
+  validates :tickets, presence: true, numericality: { greater_than: 0 }
+
 end
