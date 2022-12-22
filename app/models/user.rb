366e9a2d142486a87190497fed7fc79d0e93e44f
majaliju <majalijunyc@gmail.com>
2022-12-22 16:15:20 -0500

    imported user  model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :username, presence: :true, uniqueness: true
+  validates :password, length: { minimum: 8, maximum: 254}
+  validates_presence_of :email
+    validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+  # validates :my_email_attribute, email: true, presence: true
+
+  has_many :posts
+  has_many :concerts, through: :posts
+
+
 end
