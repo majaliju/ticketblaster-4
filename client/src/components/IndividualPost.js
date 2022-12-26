f0cfd773b301f9500320fb6dbd99ee372eb0b0cb
majaliju <majalijunyc@gmail.com>
2022-12-26 18:10:31 -0500

    individualpost succesfully rendered and updated

--- client/src/components/IndividualPost.js
+++ client/src/components/IndividualPost.js
@@ -5,18 +5,28 @@ import { Link, useNavigate } from 'react-router-dom';
 function IndividualPost({ post, concertsUsers, handleDelete }) {
   let navigate = useNavigate();
 
-  console.log('concertsUsers: ', concertsUsers);
+  const thisUser = concertsUsers.find(
+    (eachUser) => parseInt(eachUser.id) === parseInt(post.user_id)
+  );
+
+  console.log('thisUser: ', thisUser);
 
   return (
-    <div className='relative block p-8 pb-24 border-t-4 border-pink-600 rounded-sm shadow-xl'>
-      <h3 className='text-4xl font-bold'>{post.tickets}</h3>
-      <h3 className='text-4xl font-bold'>{post.tickets}</h3>
-      <p className='mt-4 text-lg font-medium text-gray-500'>{post.body}</p>
+    <div className='relative block p-8 pb-24 border-t-4 rounded-sm shadow-xl border-secondary'>
+      {post.for_sale === true ? (
+        <h3 className='text-4xl font-bold'>SELLING: {post.tickets} TICKETS</h3>
+      ) : (
+        <h3 className='text-4xl font-bold'>BUYING: {post.tickets} TICKETS</h3>
+      )}
+
+      <h3 className='text-2xl font-thin'>{thisUser.username}</h3>
+      <h3 className='text-xl font-bold'>{thisUser.email}</h3>
+      <p className='mt-4 text-lg font-medium text-accent'>{post.body}</p>
 
       <span className='absolute bottom-8 right-8'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
-          className='w-10 h-10 text-pink-600'
+          className='w-10 h-10 text-secondary'
           fill='none'
           viewBox='0 0 24 24'
           stroke='currentColor'>
