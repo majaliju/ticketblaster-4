1093b945fd3aa96da23378b2095e3fcb5e1e349e
majaliju <majalijunyc@gmail.com>
2022-12-28 17:23:18 -0500

    reverted back to serializer method

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ApplicationRecord
 
   validates :body, presence: true
   validates :tickets, presence: true, numericality: { greater_than: 0 }
-  validates :user_id, uniqueness: { scope: :concert_id }
-  validates :concert_id, uniqueness: {scope: :user_id}
+  # validates :user_id, uniqueness: { scope: :concert_id }
+  # validates :concert_id, uniqueness: { scope: :user_id }
 
 
 end
