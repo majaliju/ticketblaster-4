ace0acc601bc4d412fbb063c0f41442e6d947f31
majaliju <majalijunyc@gmail.com>
2022-12-31 18:16:02 -0500

    lots of little updates

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -4,7 +4,8 @@ class Concert < ApplicationRecord
   has_many :posts
   has_many :users, -> { distinct }, through: :posts
 
-  validates :name, presence: true
-  validates :tickets, numericality: { greater_than: 0, only_integer: true }
+  
+  validates :location, uniqueness: { case_sensitive: false }
+
 
 end
