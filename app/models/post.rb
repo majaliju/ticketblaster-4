85bde5d2159424b61292c6a464698cec13c712f7
majaliju <majalijunyc@gmail.com>
2022-12-31 16:22:28 -0500

    updated posts validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :concert
 
-  validates :body, presence: true
-  validates :tickets, presence: true, numericality: { greater_than: 0 }
-  # validates :user_id, uniqueness: { scope: :concert_id }
-  # validates :concert_id, uniqueness: { scope: :user_id }
+  validates :body, presence: true, length: { minimum: 6}
+  validates :tickets, presence: true, numericality: { greater_than: 0, only_integer: true }
+
 
 
 end
