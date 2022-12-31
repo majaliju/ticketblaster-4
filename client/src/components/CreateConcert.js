7e10daccc985acd6c778ed8cff5f6c74a96fa1fa
majaliju <majalijunyc@gmail.com>
2022-12-31 17:06:29 -0500

    createconcert -- createartist -- boht have proper error rendering capacity

--- client/src/components/CreateConcert.js
+++ client/src/components/CreateConcert.js
@@ -96,24 +96,6 @@ function CreateConcert() {
             </div>
           ) : null}
 
-          <div className='shadow-lg alert alert-warning'>
-            <div>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='flex-shrink-0 w-6 h-6 stroke-current'
-                fill='none'
-                viewBox='0 0 24 24'>
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z'
-                />
-              </svg>
-              <span>test!!!</span>
-            </div>
-          </div>
-
           <h1 className='text-2xl font-bold text-center text-white sm:text-3xl'>
             CREATE A CONCERT!
           </h1>
