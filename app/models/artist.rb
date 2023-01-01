68a6076b9c62222007e3d84f7241f520d6eca393
majaliju <majalijunyc@gmail.com>
2022-12-31 21:04:30 -0500

    succesfull concert post

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,7 +4,7 @@ class Artist < ApplicationRecord
   has_many :posts, through: :concerts
   
 
-  validates :name, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :genre, presence: true
   # validates :image, presence: true
   ## make sure its the proper image style
